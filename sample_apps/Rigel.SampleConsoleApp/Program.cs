a54b4083056a552b30d9da255873c2b521505fd0
Carlos Osoria <carlos.osoria@devfacto.com>
2013-03-16 00:56:38 -0600

    Re worked the Sample Appz into their on directory and own solutions to remove clutter on Rigel library

---
+++ sample_apps/Rigel.SampleConsoleApp/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using NLog;
+using NLog.Targets;
+using Rigel.Logging;
+
+
+namespace Rigel.SampleConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var logFactory = new NLogLoggerFactory();
+
+            var logger = logFactory.Create();
+
+            logger.LogInfo("Logging with NLog");
+
+            var target = LogManager.Configuration.AllTargets.FirstOrDefault() as FileTarget;
+
+            if (target != null)
+            {
+                Console.Out.WriteLine(String.Format("Review log file at {0} for results", target.FileName));
+            }
+            
+            Console.In.ReadLine();
+        }
+    }
+}
