0e8ce738ea5a5bd68362d5c018326811917a256a
Carlos Osoria <carlos.osoria@devfacto.com>
2013-03-18 02:46:49 -0600

    Adding the correct project for entity framework support

---
+++ source/Rigel.Data.EntityFramework/BaseDbContext.cs
@@ -0,0 +1,43 @@
+﻿using System.Data.Entity;
+
+namespace Rigel.Data.EntityFramewok
+{
+    public class BaseDbContext<TContext> : DbContext, IDbContext where TContext : DbContext
+    {
+        protected BaseDbContext() : base()
+        {
+            Database.SetInitializer<TContext>(null);
+        }
+
+        protected BaseDbContext(string nameOrConnectionString)
+            : base(nameOrConnectionString)
+        {
+            Database.SetInitializer<TContext>(null);
+        }
+
+        public void Save()
+        {
+            this.SaveChanges();
+        }
+
+        public void MarkAsAdded(IEntity entity)
+        {
+            Entry(entity).State = System.Data.EntityState.Added;
+        }
+
+        public void MarkAsModified(IEntity entity)
+        {
+            Entry(entity).State = System.Data.EntityState.Modified;
+        }
+
+        public void MarkAsDeleted(IEntity entity)
+        {
+            Entry(entity).State = System.Data.EntityState.Deleted;
+        }
+
+        public void MarkAsUnchanged(IEntity entity)
+        {
+            Entry(entity).State = System.Data.EntityState.Deleted;
+        }
+    }
+}
